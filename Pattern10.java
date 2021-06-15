1c5d5deba74e9f84a95f6fbdfc6232b747173840
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-15 22:29:37 +0530

    Add files via upload

---
+++ Pattern10.java
@@ -0,0 +1,35 @@
+/*
+Output:
+=======
+A A A A A
+B B B B B
+C C C C C
+D D D D D
+E E E E E
+*/
+
+public class Pattern10
+{
+	public static void main(String[] args)
+	
+	{
+		char ch='A';
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print(ch+" ");
+				
+
+			}
+			System.out.println();
+			ch++;			
+
+		}
+		
+
+	}
+
+
+
+}
