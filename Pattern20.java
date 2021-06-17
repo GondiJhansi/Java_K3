7c743a2c4c9e013dd3e81b85c4d9d814b3a09b64
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-17 07:31:03 +0530

    Add files via upload

---
+++ Pattern20.java
@@ -0,0 +1,29 @@
+/*
+Output:
+========
+2
+2 4
+2 4 6 
+2 4 6 8 
+2 4 6 8 10
+*/
+
+public class Pattern20
+{
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+		{   
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j*2+" ");
+			}
+			System.out.println();
+
+		}
+
+
+	}
+
+
+}
