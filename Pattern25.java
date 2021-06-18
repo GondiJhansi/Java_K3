67d2a9cbe9158d712cc5061e67459c93d28c157e
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-18 08:16:35 +0530

    Add files via upload

---
+++ Pattern25.java
@@ -0,0 +1,27 @@
+/*
+Output:
+========
+E
+D D
+C C C 
+B B B B 
+A A A A A
+*/
+
+public class Pattern25
+{
+	public static void main(String[] args)
+	{
+		char ch='E';
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(ch+" ");
+
+			}
+			System.out.println();
+			ch--;			
+		}
+	}
+}
