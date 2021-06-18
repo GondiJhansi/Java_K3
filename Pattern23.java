0fe390acb807b33a6ec059e41e8c5bce47f72779
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-18 08:15:35 +0530

    Add files via upload

---
+++ Pattern23.java
@@ -0,0 +1,31 @@
+/*
+Output:
+========
+A
+B B
+C C C 
+D D D D 
+E E E E E 
+*/
+
+public class Pattern23
+{
+	public static void main(String[] args)
+	{
+		char ch='A';
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(ch+" ");
+
+
+			}
+			System.out.println();
+			ch++;
+		}
+
+	}
+
+}
+
