da1e1cbdb55d66d6145a0c9e73c779a9508e12e9
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-18 08:13:29 +0530

    Add files via upload

---
+++ Pattern22.java
@@ -0,0 +1,37 @@
+/*
+Output:
+========
+A
+A B
+A B C
+A B C D
+A B C D E
+*/
+
+public class Pattern22
+{
+	public static void main(String[] args)
+	{
+		char ch='A';
+		for(int i=1;i<=5;i++)
+		{
+			ch='A';
+			for(int j=1;j<=i;j++)
+			{		
+				System.out.print(ch+" ");
+				ch++;
+			}
+			System.out.println();
+
+		}
+
+
+
+	}
+
+
+
+
+
+}
+
