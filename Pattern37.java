fca0a6862a89c5aada07eab62135848193b74aee
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-21 22:25:53 +0530

    Add files via upload

---
+++ Pattern37.java
@@ -0,0 +1,30 @@
+/*
+Output:
+========
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+public class Pattern37
+{
+	public static void main(String[] args)
+	{
+
+   		for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>i;j--)
+			{
+				System.out.print(" ");
+			}
+
+			for(int n=1;n<=i;n++)
+			{
+				System.out.print("*");
+			}			
+			System.out.println();
+		}
+	}
+}
