5978ff9617b19ce7784363f1e579bdc3e5fb5acd
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-22 22:04:44 +0530

    Add files via upload

---
+++ NumberFactorial.java
@@ -0,0 +1,24 @@
+/*
+Factorial of a given number
+Input:6
+Output:720
+*/
+
+import java.util.*;
+public class NumberFactorial
+{
+	public static void main(String[] args)
+	{
+		Scanner se=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=se.nextInt();
+		int f=1;
+		for(int i=1;i<=num;i++)
+		{
+			f=f*i;
+
+		}
+		System.out.println("Factorial of "+num+" = "+f);
+	}
+
+}
