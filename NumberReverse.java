d819ed759f2c132e41ef8cfe9d99da81095b4299
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-06-25 07:31:39 +0530

    Add files via upload

---
+++ NumberReverse.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class NumberReverse
+{
+	public static void main(String[] args)
+	{
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number:");
+		int num=sc.nextInt();
+		int rev=0,rem;
+		while(num!=0)
+		{
+			rem=num%10;
+			rev=rev*10+rem;
+			num=num/10;
+
+		}
+		System.out.println("Reverse is ");		
+	
+
+
+	}
+
+
+
+
+
+
+}
