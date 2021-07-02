ff43cdfaf2036efed296258b26885d551ddca282
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-02 20:05:40 +0530

    Add files via upload

---
+++ No_Words.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class No_Words
+
+{
+	public static void main(String[] rags)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Sentence:");
+		String str= sc.nextLine();
+		String words[]=str.split("\\s");
+		System.out.println("No of words = "+words.length3);
+
+	}
+} 
