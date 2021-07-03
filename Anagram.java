4ed650a099cf90e7f7e50a40193d7cc90410e6a5
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-03 19:52:57 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class Anagram
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 Strings:");
+		String str1 = sc.next();
+		String str2 = sc.next();
+		boolean f = true;
+		if(str1.length() != str2.length())
+			f=false;
+		else
+		{
+			char s1[] = str1.toCharArray();
+			char s2[] = str2.toCharArray();
+			Arrays.sort(s1);
+			Arrays.sort(s2);
+			f = Arrays.equals(s1,s2);
+		}
+		if(f)
+			System.out.println("Anagram");
+		else
+			
+			System.out.println("Not Anagram");
+	}
+}
+
+		
