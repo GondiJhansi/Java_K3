bc9f140f7b42b979efc275e50b402ce274bbc32f
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-04 19:49:45 +0530

    Add files via upload

---
+++ PalindromeStr.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class PalindromeStr
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String:");
+		String str = sc.next();
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+
+		}
+		if(str.equals(rev))
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not Palindrome");
+	}
+}			
+	
