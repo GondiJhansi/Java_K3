641f4e0ccad4aa7ba60f337f99bd5fc3e3efff62
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-06 19:45:41 +0530

    Add files via upload

---
+++ Init_Values.java
@@ -0,0 +1,12 @@
+
+public class Init_Values
+{
+ 	public static void main(String[] args)
+	{
+		int a[] = {10,20,30,40,50};
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("a["+i+"] = "+a[i]);
+		}
+	}
+} 
