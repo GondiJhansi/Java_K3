b35c669252a971526f80176f6c56e835343fe33d
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-07 19:33:58 +0530

    Add files via upload

---
+++ Init_2D.java
@@ -0,0 +1,21 @@
+
+public class Init_2D
+{
+	public static void main(String[] args)
+	{
+		int a[][] = {{10,20,30},{30,40,50}};
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}		
+			System.out.println();
+
+		}
+
+
+	}
+
+
+}
