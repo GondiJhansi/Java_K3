3a62cbd1a43eca7babc91152ced7782e3679d772
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-11 22:36:54 +0530

    Add files via upload

---
+++ stdin_out.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Stdin_Out 
+{
+	public static void main(String[] args) 
+	{
+        	Scanner scan = new Scanner(System.in);
+		System.out.println("Enter numbers:");
+        	int a = scan.nextInt();
+        	int b = scan.nextInt();
+        	int c = scan.nextInt();
+        	System.out.println(a);
+        	System.out.println(b);
+        	System.out.println(c);
+    	}
+}
