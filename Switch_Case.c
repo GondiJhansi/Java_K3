a8f7a14a489642ed66ab9b6481572b46aeed328c
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-23 07:01:09 +0530

    Add files via upload

---
+++ Switch_Case.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a choice from 1 to 7");
+	scanf("%d",&num);
+	switch(num)
+	{
+		case 1 : printf("Sunday");
+				 break;
+		case 2 : printf("Monday");
+				 break;
+		case 3 : printf("Tuesday");
+				 break;
+		case 4 : printf("Wednesday");
+				 break;
+		case 5 : printf("Thursday");
+				 break;
+		case 6 : printf("Friday");
+				 break;
+		case 7 : printf("Saturday");
+				 break;
+		default : printf("You entered wrong choice");
+		
+		
+	}
+	return 0;
+	
+}
