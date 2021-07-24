aa470d5aa6e8a609fb8ee473e4528ec9ecd88f02
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-07-24 08:03:22 +0530

    Add files via upload

---
+++ Shift_Op.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=6,b=5;
+	printf("%d << 2 = %d\n",a,a<<2);
+	printf("%d >> 2 = %d\n",a,a>>2);
+	printf("%d << 2 = %d\n",b,b<<2);
+	printf("%d >> 2 = %d\n",b,b>>2);
+	return 0;
+}
